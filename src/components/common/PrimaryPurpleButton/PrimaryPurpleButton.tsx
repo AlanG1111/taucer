9d999dc02d137b886f5a8c2b4f2799aeed1445ac
Alan <alan_h@evacodes.com>
2025-01-02 16:18:01 +0200

    add primary purple button

---
+++ src/components/common/PrimaryPurpleButton/PrimaryPurpleButton.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './PrimaryPurpleButton.module.scss';
+
+type Props = {
+  title: string;
+  onClick: () => void;
+  disabled?: boolean; 
+};
+
+const PrimaryPurpleButton: React.FC <Props> = ({ title, onClick, disabled = false  }) => {
+  return (
+    <button
+      className={`${styles.animatedButton}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <span>{title}</span>
+    </button>
+  );
+};
+
+export default PrimaryPurpleButton;
