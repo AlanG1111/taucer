82c33c86e1378cca992cf0cbecbf60b42e735d25
Alan <alan_h@evacodes.com>
2025-01-02 18:30:06 +0200

    home page request container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
+
+import HomePageRequestContainer from '@/components/HomePageRequestContainer/HomePageRequestContainer';
 import MainIntro from '@/components/MainIntro/MainIntro';
 
 export default function Home() {
   return (
     <div>
       <MainIntro />
+      <HomePageRequestContainer />
     </div>
   );
 }
